1dc7eacfec6949ebeb508dc4eab7db0a43b063d1
JoanMouba <43171207+JoanMouba@users.noreply.github.com>
2020-06-18 15:32:55 +0200

    JM: add arguments in setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,18 @@
 # SETUP file 
-# Package and distribution management 
-# Development environment setup 
+# Package and distribution management and Development environment setup 
+
+from setuptools import setup, find_packages
+setup(
+  name="SafeProsper",
+  version=0.1,
+  packages=find_packages(),
+  install_requires=["docutils>=0.3"],
+  test_suite="tests",
+  package_data={
+    "":["*.txt", "*.rst"],
+  },
+  author="Joan Mouba", 
+  author_email="joan.mouba@gmail.com",
+  descrtiption="Package to help you manage your personal finances",
+  url="https://sunesiscenter.wixsite.com/accueil"
+)
